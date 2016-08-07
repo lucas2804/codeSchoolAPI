cd544af7773113b1180cbd2e0c7af3a57353b204
Luc <luc.dangquang@rainmaker-labs.com>
2016-08-07 13:44:13 +0700

    update wrap REST API into api_folder

--- test/integration/creating_crawlers_test.rb
+++ test/integration/creating_crawlers_test.rb
@@ -2,17 +2,18 @@ require 'test_helper'
 
 class CreatingCrawlersTest < ActionDispatch::IntegrationTest
   test 'creates new crawler with valid data' do
-    post '/crawlers',
+    post '/api/crawlers',
          {crawler: {url: "http://google.com", h1_content: "H1 in google.com"}}.to_json,
          {Accept: 'application/json', 'Content-Type' => 'application/json'}
+
     assert_equal 201, response.status
     crawler = JSON.parse(response.body, symbolize_names: true)
-    assert_equal crawler_url(crawler[:id]), response.location
+    assert_equal api_crawler_url(crawler[:id]), response.location
     assert_equal "http://google.com", crawler[:url]
   end
 
   test 'does not creates crawler with INVALID data' do
-    post '/crawlers',
+    post '/api/crawlers',
          {crawler: {url: "", h1_content: "H1 in google.com"}}.to_json,
          {Accept: 'application/json', 'Content-Type' => 'application/json'}
     assert_equal 422, response.status
