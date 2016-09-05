e54cbae9c7e92b1a7c0ab8a6a40aecddd1291ccd
Luc <luc.dangquang@rainmaker-labs.com>
2016-09-05 12:23:09 +0700

    update validate belongs_to required for test_result

--- app/models/test_result.rb
+++ app/models/test_result.rb
@@ -1,8 +1,8 @@
 class TestResult < ActiveRecord::Base
-  has_many :questions
-  has_many :tests
-  has_many :users
-  has_many :answers
+  belongs_to :question, required: true
+  belongs_to :test
+  belongs_to :user, required: true
+  belongs_to :answer
 
   validates :question_id, :test_id, :user_id, :answer_id, presence: true
   validates_uniqueness_of :question_id, scope: :user_id
