3f84b32276fbfbcf9c3c7c0102bbd1859a445b01
Luc <luc.dangquang@rainmaker-labs.com>
2016-09-05 10:55:27 +0700

    remove provider nad uuid column

--- db/migrate/20160830142554_devise_create_users.rb
+++ db/migrate/20160830142554_devise_create_users.rb
@@ -1,10 +1,6 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
-      ## Required
-      t.string :provider, :null => false, :default => "email"
-      t.string :uid, :null => false, :default => ""
-
       ## Database authenticatable
       t.string :encrypted_password, :null => false, :default => ""
 
